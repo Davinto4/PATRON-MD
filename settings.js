2cf7bbbc357af54872943bd3b0860b0e7a0ceca4
Davinto4 <eroticoden@gmail.com>
2025-03-02 06:33:07 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "Palmpay"
 
-global.banknumber = "8133729715"
+global.banknumber = "8169045105"
 
-global.bankowner = "FADARE"
+global.bankowner = "Davinto"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
